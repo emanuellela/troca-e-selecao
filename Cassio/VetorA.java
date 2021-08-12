450ef44634da7a6994cb5aa7f2ad5953ad3a9110
CassioJr <cassiojuniorceolin@gmail.com>
2021-08-12 19:53:47 -0300

    Vetor A e Vetor B feitos

--- Cassio/VetorA.java
+++ Cassio/VetorA.java
@@ -1,15 +1,19 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class VetorB {
+public class VetorA {
 public static void main(String args[]) {
 	Scanner sc = new Scanner(System.in);
 	int n = sc.nextInt();
-	int vet[]= new int[n];
+	int vet[] = new int[n];;
+	int porcentagem = (n * 90) / 100;
+	int resto = n-porcentagem;
 	Random gerador = new Random();
-	
 	for(int i=0;i<n;i++) {
-	vet[i] = gerador.nextInt(n*n);
-	}
+		vet[i] = 1;
+		}
+	for(int i=0;i<resto;i++) {
+		vet[i]= gerador.nextInt(porcentagem);
+}
 }
 }
