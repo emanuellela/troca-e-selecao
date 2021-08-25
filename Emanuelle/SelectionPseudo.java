918c235f42ec5a8c9007f8921673ee1fbf10ab91
1901570182 <emanuelleleaes@gmail.com>
2021-08-25 15:53:03 -0300

    selection sort pseudocódigo

---
+++ Emanuelle/SelectionPseudo.java
@@ -0,0 +1,15 @@
+selectionSort(P,n)
+{
+	para i <- 0 até n-2
+	{
+        min <- i
+        para j <- i + 1 até n - 1
+        {
+            se (P[j]<P[min])
+                min <- j
+        }
+		troca <- P[i]
+		P[i] <- P[min]
+		P[min] <- troca
+	}
+}    
